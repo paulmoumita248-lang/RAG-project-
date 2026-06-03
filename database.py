bc1d434c8843d98af380c7a165fb13501eb60290
Moumita Paul <paulmoumita248@gmail.com>
2026-06-03 22:44:18 +0530

    creating Project
    here in app.py you don't have the option for uploding fill you have to ask question from already uploded document

---
+++ database.py
@@ -0,0 +1,30 @@
+#load pdf 
+#split into chunks 
+#create the embeddings 
+#store into chroma 
+from google import genai
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+data = PyPDFLoader("Documentloder/Deeplearning.pdf")
+docs = data.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap = 200
+)
+
+chunks = splitter.split_documents(docs)
+
+embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+vectorstore = Chroma.from_documents(
+    documents= chunks,
+    embedding=embedding_model,
+    persist_directory="chroma_db"
+)
